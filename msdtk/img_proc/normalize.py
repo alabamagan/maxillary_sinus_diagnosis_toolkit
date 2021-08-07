05c6a0a5af2a91a1328049139d498b8ec43635f1
alabamagan <fromosia@gmail.com>
2021-08-07 15:25:00 +0800

    Add function to normalize the intensity of the images.

--- msdtk/img_proc/normalize.py
+++ msdtk/img_proc/normalize.py
@@ -3,13 +3,18 @@ import SimpleITK as sitk
 import nibabel
 import os
 import numpy as np
-from pytorch_med_imaging.logger import Logger
+import multiprocessing as mpi
+import torchio as tio
+from pathlib import Path
 
-__all__ = ['resample_to_standard_spacing']
+from pytorch_med_imaging.logger import Logger
+from typing import Union, Callable, Iterable, Optional
+
+__all__ = ['resample_to_standard_spacing', 'intensity_normalization_inference', 'intensity_normalization_train']
 
 def resample_to_standard_spacing(in_fname: str,
                                  out_dir: str,
-                                 seg_fname: str = None) -> None:
+                                 seg_fname: Optional[str] = None) -> None:
     r"""
     Resample the input NIFTI file to standard spacing (0.4 mm)
 
@@ -87,10 +92,116 @@ def resample_to_standard_spacing(in_fname: str,
                 logger.warning(f"{seg_fname} was not linked.")
 
 
-def intensity_normalization():
+def intensity_normalization_train(imgs_dir: Union[str, Path, Iterable[Union[str, Path]]],
+                                  out_landmark_path: Union[str, Path],
+                                  mask: Optional[Union[str, Iterable[Union[str, Path]], Callable]] = None,
+                                  recurisve_include: Optional[bool] = False) -> None:
     r"""
     Use the torchio API to perform histogram normalization
     """
-    import torchio as tio
+    import fnmatch
 
+    out_landmark_path = Path(out_landmark_path)
+    if not out_landmark_path.parent.is_dir():
+        out_landmark_path.parent.mkdir(exist_ok=True)
+
+    if recurisve_include:
+        imgs = []
+        for r, d, f in os.listdir(imgs_dir):
+            if len(f):
+                f = fnmatch.filter(f, "*.nii.gz")
+                imgs.extend([os.path.join(r, ff) for ff in f])
+    else:
+        imgs = os.listdir(imgs_dir.absolute())
+
+    masking_function = mask if callable(mask) else None
+    masking_path = mask if isinstance(mask, [list, tuple]) else None
+    if not masking_path is None:
+        if len(masking_path) != len(imgs):
+            raise IndexError("Length of images and mask are different")
+    if mask == 'corner': # Use corner pixel as background
+        masking_function = lambda x: x > x.flatten()[0]
+
+
+    hist_norm = tio.transforms.HistogramStandardization.train(imgs, masking_function=masking_function, masking_path=masking_path)
+    np.save(out_landmark_path)
     pass
+
+def intensity_normalization_inference(imgs_dir: Union[str, Path, Iterable[Union[str, Path]]],
+                                      output_dir: Union[str, Path],
+                                      landmark_path: Union[str, Path],
+                                      mask: Optional[Union[str, Callable, None]] = None,
+                                      recursive_include: Optional[bool] = False,
+                                      numworker: Optional[int] = 16) -> None:
+    r"""
+    Perform normalization
+    """
+    import fnmatch
+    from functools import partial
+
+    # Error check
+    landmark_path = Path(landmark_path)
+    assert Path.is_file(landmark_path), f"Cannot onpen landmark at {landmark_path}"
+    if isinstance(output_dir, str): # Make sure this is a directory
+        if not output_dir.endswith(os.path.sep):
+            output_dir += os.path.sep
+
+
+    logger = Logger['intensity_normalization']
+
+    if recursive_include:
+        imgs = []
+        for r, d, f in os.listdir(imgs_dir):
+            if len(f):
+                f = fnmatch.filter(f, "*.nii.gz")
+                imgs.extend([os.path.join(r, ff) for ff in f])
+    else:
+        imgs = os.listdir(Path(imgs_dir).absolute())
+        imgs = fnmatch.filter(imgs, '*.nii.gz')
+        imgs = [os.path.join(imgs_dir, ff) for ff in imgs]
+
+
+    logger.info(f"Found {len(imgs)} files: \n{imgs}")
+
+    logger.info(f"Loading landmarks from: {landmark_path}")
+    landmarks = {'image': np.load(landmark_path)}
+
+    if mask == 'corner': # Use corner pixel as background
+        masking_function = _corner_masking
+    else:
+        masking_funciton = None
+
+
+
+    logger.info("Performing transform...")
+    if numworker == 1:
+        for f in imgs:
+            _normalization_transform(f, landmarks, imgs_dir, output_dir, masking_function)
+    elif numworker > 1:
+        logger.info("MPI mode")
+        process = []
+        pool = mpi.Pool(numworker)
+        for f in imgs:
+            p = pool.apply_async(_normalization_transform,
+                                 args=[f, landmarks, imgs_dir, output_dir, masking_function])
+            process.append(p)
+        pool.close()
+        pool.join()
+
+
+def _normalization_transform(f, landmarks, imgs_dir, output_dir, masking_function):
+    r"""MPI Wrapping function"""
+    logger = Logger['msdtk-'+mpi.current_process().name]
+    hist_norm = tio.transforms.HistogramStandardization(landmarks, masking_method=masking_function)
+
+    logger.info(f"Processing: {f}")
+    s = tio.Subject(image=tio.ScalarImage(f))
+    s_normed = hist_norm(s)
+    out_name = Path(f.replace(imgs_dir, output_dir).replace('.nii.gz', '_normed.nii.gz'))
+    if not out_name.parent.is_dir():
+        out_name.parent.mkdir(exist_ok=True)
+    logger.info(f"Writing to: {out_name}")
+    s_normed.image.save(out_name)
+
+def _corner_masking(x):
+    return x > x.flatten()[0]
