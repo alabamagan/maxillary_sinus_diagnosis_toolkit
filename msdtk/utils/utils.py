577949dd6efdfc0c82b3b76d01b0433256d2d9ea
alabamagan <fromosia@gmail.com>
2021-08-09 22:26:02 +0800

    Add a function to get files using a list of ids.

--- msdtk/utils/utils.py
+++ msdtk/utils/utils.py
@@ -2,7 +2,7 @@ import pandas as pd
 from pathlib import Path
 from typing import List, Union, Iterable, Optional, Pattern
 
-__all__ = ['norm_df_cols', 'get_ids_from_files']
+__all__ = ['norm_df_cols', 'get_ids_from_files', 'get_files_with_id']
 
 def norm_df_cols(df:pd.DataFrame, cols: List[str]):
     r"""Normalize the row across multiple columns. Change the data frame inplace."""
@@ -34,7 +34,7 @@ def get_ids_from_files(files: Iterable[Union[str, Path]],
 
     if isinstance(files, str):
         if not Path(files).is_dir():
-            raise FileNotFoundError(f"Specified file is not a directory: {files}")
+            raise OSError(f"Specified file is not a directory: {files}")
         files = fnmatch.filter(os.listdir(files), '*.nii.gz')
 
     out_dict = {}
@@ -54,3 +54,46 @@ def get_ids_from_files(files: Iterable[Union[str, Path]],
         return out_dict
     else:
         return list(out_dict.keys())
+
+def get_files_with_id(directory: Union[str, Path],
+                      idlist: List[str],
+                      idglobber: Optional[Pattern] = "^[0-9]+",
+                      return_dict: Optional[bool] = False):
+    r"""Get a list of files using ID
+
+    Args:
+        directory (str):
+            Where you want to get the images from.
+        idlist (list or str):
+            The desired image IDs.
+        idglobber (Optional pattern):
+            ID globbing regex.
+        recursive (Optional, bool):
+            NOT IMPLEMENTED. Whether or not to search recursively.
+
+    Returns:
+        list of str
+    """
+    import os
+    import re, fnmatch
+
+    if not Path(directory).is_dir():
+        raise OSError(f"Cannot open specified directory: {directory}")
+
+    files = fnmatch.filter(os.listdir(directory), '*.nii.gz')
+    files_id = get_ids_from_files(files, idglobber=idglobber, return_dict=True)
+
+    out_dict = {}
+    for keys in idlist:
+        _val = files_id.get(keys, None)
+        if isinstance(_val, (list, tuple)):
+            print("Warning, there are multiple files with the same ID.")
+        if _val is None:
+            print(f"Warning, can't find file with ID {keys} in target folder.")
+        out_dict[keys] = _val
+
+    if return_dict:
+        return out_dict
+    else:
+        return list([out_dict[keys] for keys in out_dict])
+
